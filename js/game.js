576016af149a6238033ec3a3414314ac97d84943
kennethyang404 <njrobby@163.com>
2015-03-03 01:46:06 -0500

    update game value

--- js/game.js
+++ js/game.js
@@ -48,14 +48,14 @@ function handleComplete() {
     sheep.y=h-1;
 
     ball = new createjs.Bitmap(loader.getResult("ball1"));
-    ball.desireX=sheep.desireX/1.5;
+    ball.desireX=sheep.desireX/1.3;
     ball.desireY=ball.desireX;
     ball.scaleX=ball.desireX / ball.image.width;
     ball.scaleY=ball.scaleX;
     ball.regX=ball.image.width / 2;
     ball.regY=ball.image.height;
     ball.x=w/2;
-    ball.y=h/2;
+    ball.y=h/3;
     ball.visible = true;
 
     superball = new createjs.Bitmap(loader.getResult("ball2"));
@@ -66,7 +66,7 @@ function handleComplete() {
     superball.regX=ball.image.width / 2;
     superball.regY=ball.image.height;
     superball.x=w/2;
-    superball.y=h/2;
+    superball.y=h/3;
     superball.visible = false;
 
     createjs.Ticker.framerate = 60;
@@ -94,16 +94,15 @@ function setPhysics() {
     sheep.v = 0;
     sheep.state="up";
     sheep.maxHeight = h - sheep.desireX/1.3;
-    sheep.criticalH = h - sheep.desireX/1.4;
+    sheep.criticalH = h - sheep.desireX/1.34;
     sheep.initV = sheep.maxHeight / (createjs.Ticker.framerate * 1.2);
     sheep.v=sheep.initV;
 
-    ball.maxHeight = h/1.8;
-    ball.initV = sheep.initV * 2.5;
-    ball.superV = ball.initV * 1.5;
+    ball.maxHeight = h/2;
+    ball.initV = sheep.initV * 2.6;
+    ball.superV = ball.initV * 1.8;
     ball.a = ball.initV * ball.initV / (2 * ball.maxHeight);
     ball.v = -Math.sqrt(2*ball.a*(ball.maxHeight/2));
-    ball.maxHeight += sheep.desireY;
     ball.state = "down";
 }
 
@@ -124,7 +123,7 @@ function move() {
         }
         if (sheep.y<=sheep.criticalH) {
             ball.state="superUp";
-            ball.v=ball.superV;
+            ball.v=ball.superV*(1+Math.random()/5);
             superball.visible = true;
             ball.visible = false;
             combo += 1;
