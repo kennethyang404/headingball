b2a14151c146f8eb5e6341da93d81978f1c4d9a1
kennethyang404 <njrobby@163.com>
2015-03-04 04:08:12 -0500

    add baidu analytics

---
+++ js/analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?dc21481e31adaadbf8c3c5266d9860d1";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
